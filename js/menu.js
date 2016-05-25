45685a8a1c103749b7fcf7f0e5ef464ac21e8f3d
Tfff1 <tefinlay2011@hotmail.com>
2016-05-25 15:55:20 +1200

    Bug Fixes

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
 $(document).ready(function tabSetter() {
-    if($('#loaded').is(':visible')){
-        console.log("Setting Active Tab...");
+    try {
         var mystring = location.pathname.split(".").slice(0, -1);
-        document.getElementById(mystring).setAttribute("class", "active"); 
-    } else {
-        setTimeout(tabSetter, 50); //wait 50 ms, then try again
+        document.getElementById(mystring).setAttribute("class", "active");
+        console.log("Set Active Tab...");
     }
+    catch (err) {
+        setTimeout(tabSetter, 50); //wait 50 ms, then try again
+        }
+    
 });
